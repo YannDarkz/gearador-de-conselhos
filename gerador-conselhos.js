263ba1e6fe35af52cfd2d776454b47078c6b2ccd
yanndarkz <iansantosmcqueen@gmail.com>
2026-03-28 00:55:44 -0300

    feat: active feature traduction

--- gerador-conselhos.js
+++ gerador-conselhos.js
@@ -1,21 +1,34 @@
-alert('olá mundo')
 
 const botaoGerador = document.getElementById("btnGerador");
 const idConselho = document.querySelector(".advice-id");
 const conselho = document.querySelector(".advice-description");
-console.log(idConselho)
+
+async function traduzirTexto(texto) {
+    const response = await fetch("https://api.mymemory.translated.net/get?q=" + texto + "&langpair=en|pt");
+    const data = await response.json();
+    return data.responseData.translatedText;
+}
 
 async function getAdvice() {
     const response = await fetch("https://api.adviceslip.com/advice");
     const adviceContent = await response.json();
-    const adviceId = `Advice #${adviceContent.slip.id}`;
-    const adviceText = `"${adviceContent.slip.advice}"`;
+    const adviceId = `Conselho ${adviceContent.slip.id}`;
+    const adviceText = `${adviceContent.slip.advice}`;
 
+    const textoTraduzido = await traduzirTexto(adviceText);
+    const idTraduzido = await traduzirTexto(adviceId);
+    console.log("id", adviceId );
+
+    
+
+    // const traductionText = await traduzirTexto(adviceText)
+    
     idConselho.innerHTML = adviceId;
-    conselho.innerHTML = adviceText;
+    conselho.innerHTML = textoTraduzido;
+    console.log("adviceContent: ", adviceContent);
 }
 
 botaoGerador.addEventListener("click", getAdvice);
 
- getAdvice();
+//  getAdvice();
 
